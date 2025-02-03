978e3ca8d4ffea43f95ad3f281abd38e28113919
NEHA688 <tneha1052@gmail.com>
2025-02-03 21:29:20 +0530

    number.py

--- number.py
+++ number.py
@@ -11,3 +11,4 @@ num3 = float(input("Enter third number: "))
 # Finding and displaying the greatest number
 greatest = find_greatest(num1, num2, num3)
 print(f"The greatest number is: {greatest}")
+# This is my second repo
